ae0932c89f767fa7d99adba1e01d08ee3ca8f8c5
wengdahl <wfengdahl@wpi.edu>
2022-02-16 23:30:17 -0500

    Fixed successor generation code

--- Assignment 1/A-star.py
+++ Assignment 1/A-star.py
@@ -1,13 +1,14 @@
 import csv
-import queue
 import heapq
+import random
+import numpy
 
 #@adeline please fill in these functions:
 def heuristic(b):
-    return 4
+    return random.randint(1, 9)
 
 def cost(base,new):
-    return 0
+    return random.randint(1, 9)
 
 # ' Wrapper for a HeapQ, mainly provides an "exists" function that tells wheteher a board exists in queue
 class queueTools:
@@ -20,10 +21,15 @@ class queueTools:
     def get(self):
         return heapq.heappop(self.h)
 
+    #Yes, I copied this from stackoverflow: https://stackoverflow.com/questions/18181818/python-priority-queue-checking-to-see-if-item-exists-without-looping
+    #and it doesnt work, need to figure out a reasonable check
     def exists(self, item):
         return item in (x[1] for x in self.h)
 
+    def isEmpty(self):
+        return len(self.h)==0
 
+size = 0
 # load board
 with open('HeavyQBoards/test.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
@@ -36,26 +42,52 @@ with open('HeavyQBoards/test.csv', newline='') as csvfile:
     print(size)
 
 frontier = queueTools()
+closed = queueTools();
 # add 1st node
-cost = 0+heuristic(array)
-frontier.add(cost, array)
+est_cost = 0+heuristic(array)
+frontier.add(est_cost, array)
+
+print(not frontier.isEmpty())
 
 # while not solved
-# while True:
+while True:#not (frontier.isEmpty): #no f-cking clue why this doesnt run, when the print above it does
 # open lowest cost
-b = frontier.get()[1];
-print(b);
-print(frontier.exists(b))
-# switch it to closed
+    print("get")
+    b = frontier.get();
+    openBoard = b[1]
+    print(b);
+    print(frontier.exists(b))
+    # switch it to closed
+    closed.add(b[0], [b[1]])
 
+    n=0
+    n2=0
+    # Generate successors - I think this works well (or at least passably, not 100% sure), except that the comparison is always false , not actually checking array elements
+    for i in range(0,size):#each column
+        print("arr"+str(i))
+        pos = 0
+        val = 0
+        for k in range(0, size):
+            if int(openBoard[k][i]) != 0:
+                pos = k
+                val = openBoard[k][i]
+                print("VAL: "+str(val)+" at "+str(k)+" "+str(i))
+                break
+        for j in range(0,size): #each row
+            if j == pos:                continue
+            successor = openBoard
+            successor[j][i] = val
+            successor[pos][i] = 0
+            n=n+1
+            print("SUC "+str(n)+" " +str(successor))
+            if not closed.exists(successor):#this is the part that doesn't work
+                n2=n2+1
+                est_cost = cost(array, successor) + heuristic(successor)
+                frontier.add(est_cost, successor)
 
-# Generate successors
-# for i in range(1,size):
-# total est = getMoveCost(array,successor)+heuristic(successor)
-# print(i,array[i][i]);
-
-# If solved exit
-# break
+    print("Successors "+str(n)+" added "+str(n2))
+    # If solved exit
+    # break
 
 # Test code
 # q.put( (10, 4) )
