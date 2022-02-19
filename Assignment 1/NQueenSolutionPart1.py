100657729407467ddcef1562f2c5a747cf0365bf
Amitai5 <aberfanian@outlook.com>
2022-02-19 13:17:35 -0500

    Updated output and formatted file

--- Assignment 1/NQueenSolutionPart1.py
+++ Assignment 1/NQueenSolutionPart1.py
@@ -1,27 +1,17 @@
 import csv
-import heapq
-import random
-import numpy
-import copy
 import sys
-
-#@adeline please fill in these functions:
+import copy
+import heapq
+import numpy
+import random
 import numpy as np
 
 
 def heuristic(b):
-    v = moveVector(b) #move vector and row vector
-    #w = weightVector(b) #weight vector
-    d1 = diag1Vector(v) #diag1 vector
-    d2 = diag2Vector(v) #diag2 vector
-
-   # print("move vector: ", v)
-    #print("weight vector: ", w)
-    #print("d1 vector: ", d1)
-   # print("d2 vector: ", d2)
-
+    v = moveVector(b)  # move vector and row vector
+    d1 = diag1Vector(v)  # diag1 vector
+    d2 = diag2Vector(v)  # diag2 vector
     hc = 0
-
     for i in range(size):
         for j in range(1, size - i):
             if v[i] == v[j + i]:
@@ -30,27 +20,17 @@ def heuristic(b):
                 hc = hc + w[i] + w[j + i]
             if d2[i] == d2[i + j]:
                 hc = hc + w[i] + w[j + i]
-    #print("hc: ", hc)
     return hc
 
-def cost(base,new):
+
+def cost(base, new):
     b = moveVector(base)
     n = moveVector(new)
-    #w = weightVector(base)
-
-    #print("Base: ", base)
-    #print("new: ", new)
-
-    #print("b: ", b)
-    #print("n: ", n)
-
     moveCost = np.absolute(np.array(b) - np.array(n))
-    #print("Move cost: ", moveCost)
-   # print("w^2: ", np.square(np.array(w)))
     moveCost = np.multiply(moveCost, np.square(np.array(w)))
     moveCost = moveCost.sum()
-
     return moveCost
+
 
 def getBoardString(b):
     mv = moveVector(b)
@@ -59,14 +39,15 @@ def getBoardString(b):
         bString = bString + str(i)
     return bString
 
+
 def moveVector(b):
     x = []
     for i in range(size):
         for j in range(size):
             if b[j][i] != 0:
                 x.append(j)
-    #print("Move vector: ", x)
     return x
+
 
 def weightVector(b):
     x = []
@@ -74,25 +55,24 @@ def weightVector(b):
         for j in range(size):
             if b[j][i] != 0:
                 x.append(int(b[j][i]))
-    #print(x)
     return x
+
 
 def diag1Vector(mv):
     d1 = []
     for i in range(size):
         d1.append(i - mv[i])
-    #print(d1)
     return d1
+
 
 def diag2Vector(mv):
     d2 = []
     for i in range(size):
         d2.append(i + mv[i])
-    #print(d2)
     return d2
 
 
-# ' Wrapper for a HeapQ, mainly provides an "exists" function that tells wheteher a board exists in queue
+# Wrapper for a HeapQ, mainly provides an "exists" function that tells wheteher a board exists in queue
 class queueTools:
     def __init__(self):
         self.h = []
@@ -107,72 +87,65 @@ class queueTools:
         return len(self.h)
 
 
-#Main code
+# Main code
 greedy = int(sys.argv[1])
 
 size = 0
 w = []
+
 # load board
 with open('HeavyQBoards/test.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
-
     array = list(reader)
-    #array = int(array)
-
-
-
     size = len(array)
 
     for i in range(0, size):
         for j in range(0, size):
             array[i][j] = int(array[i][j])
-            print(array[i][j])
 
-    print(array)
     print("Size ", size)
-    print(range(size))
     w = weightVector(array)
-    print(w)
+    print("Weight Vector: ", w)
 
 frontier = queueTools()
-closed = queueTools();
-solution = (-1,array)
-closedList = [];
+closed = queueTools()
+solution = (-1, array)
+closedList = []
+
 # add 1st node
-est_cost = 0+heuristic(array)
+est_cost = 0 + heuristic(array)
 frontier.add(est_cost, array)
 closedList.append(getBoardString(array))
 
 # while not solved
 while frontier.len():
-# open lowest cost
-    print("get")
-    b = frontier.get();
+    print("\nget")  # open lowest cost
+    b = frontier.get()
     if b is None:
         print("Search over")
         exit()
     if solution[0] != -1 and (b[0] > solution[0] or greedy == 1):
-        print("Search complete\n")
+        print("Search Complete...")
+        print("\n\n---------------Search Results---------------")
         if greedy == 1:
             print("Greedy search")
-            print("Cost: " + str(cost(array,solution[1])) + "\n")
+            print("Cost: " + str(cost(array, solution[1])) + "\n")
         else:
             print("A* search")
-            print("Greedy: "+str(sys.argv[1]))
-            print("Cost: "+str(solution[0])+"\n")
-        print("Board:\n"+str(solution[1]))
+            print("Cost: " + str(solution[0]))
+        print("Board: " + str(solution[1]))
         np.savetxt("HeavyQBoards/ANSWER.csv", solution[1], fmt='%i', delimiter=',')
         exit()
     openBoard = b[1]
-    print(b);
-    #print(frontier.exists(b))
+    print(b)
     # switch it to closed
     closed.add(b[0], [b[1]])
 
-    n=0
-    n2=0
-    # Generate successors - I think this works well (or at least passably, not 100% sure), except that the comparison is always false , not actually checking array elements
-    for i in range(0,size):#each column
+    n2 = 0
+    n = 0
+
+    # Generate successors
+    for i in range(0, size):  # each column
         pos = 0
         val = 0
         for k in range(0, size):
@@ -180,112 +153,30 @@ while frontier.len():
                 pos = k
                 val = openBoard[k][i]
                 break
-        for j in range(0,size): #each row
+        for j in range(0, size):  # each row
             if j == pos:
                 continue
             successor = copy.deepcopy(openBoard)
             successor[j][i] = val
             successor[pos][i] = 0
-            n=n+1
-            #print("SUC "+str(n)+" " +str(successor))
+            n = n + 1
             sucString = getBoardString(successor)
-            if not sucString in closedList:#Works now
+
+            if not sucString in closedList:  # Works now
                 closedList.append(sucString)
-                n2 = n2+1
+                n2 = n2 + 1
                 h = heuristic(successor)
                 c = 0
                 if greedy != 1:
-                    #print("A*")
                     c = cost(array, successor)
-                print("h: "+str(h)+" c: "+str(c))
                 if h == 0:
                     print("FOUND")
-                    #Replace solution if cheaper
-                    if solution[0] == -1 or c<solution[0]:
+                    if solution[0] == -1 or c < solution[0]:  # Replace solution if cheaper
                         solution = (c, successor)
                 else:
                     est_cost = c + h
                     frontier.add(est_cost, successor)
 
-    print("Successors "+str(n)+" added "+str(n2))
+    print("Successors " + str(n) + " added " + str(n2))
     # If solved exit
     # break
-
-# Test code
-# q.put( (10, 4) )
-# q.put( (10, 2) )
-# q.put( (1, 3) )
-# q.put( (2, 6) )
-# while not q.empty():
-#   print(q.get())
-
-
-#<<<<<<< HEAD
-# def solveQueens(self, n: int) -> List[List[str]]:
-#     col = set()
-#     posDiag = set()  # (r + c)
-#     negDiag = set()  # (r - c)
-#
-#
-# res = []
-# board = [["."] * n for i in range(n)]
-#
-#
-# def backtrack(r):
-#     if r == n:
-#         copy = ["".join(row) for row in board]
-#         res.append(copy)
-#         return
-#
-#         for c in range(n):
-#             if c in col or (r + c) in posDiag or (r - c) in negDiag:
-#                 continue
-#
-#             col.add(c)
-#             posDiag.add(r + c)
-#             negDiag.add(r - c)
-#             board[r][c] = "Q"
-#
-#             backtrack(r + 1)
-#
-#             col.remove(c)
-#             posDiag.remove(r + c)
-#             negDiag.remove(r - c)
-#             board[r][c] = "Q"
-#     backtrack(0)
-#     return res
-# =======
-# def solveQueens(self, n: int) -> list[list[str]]:
-#     col = set()
-#     posDiag = set()  # (r + c)
-#     negDiag = set()  # (r - c)
-#
-#
-# res = []
-# board = [["."] * n for i in range(n)]
-#
-#
-# def backtrack(r):
-#     if r == n:
-#         copy = ["".join(row) for row in board]
-#         res.append(copy)
-#         return
-#
-#         for c in range(n):
-#             if c in col or (r + c) in posDiag or (r - c) in negDiag:
-#                 continue
-#
-#             col.add(c)
-#             posDiag.add(r + c)
-#             negDiag.add(r - c)
-#             board[r][c] = "Q"
-#
-#             backtrack(r + 1)
-#
-#             col.remove(c)
-#             posDiag.remove(r + c)
-#             negDiag.remove(r - c)
-#             board[r][c] = "Q"
-#     backtrack(0)
-#     return res
-# >>>>>>> main
