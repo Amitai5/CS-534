91230f423631d261f7ed11605df2d64716f3ba68
adelinemae <98119483+adelinemae@users.noreply.github.com>
2022-02-20 14:16:52 -0500

    genetic alg method stub stuff

--- Assignment 1/GeneticAlg.py
+++ Assignment 1/GeneticAlg.py
@@ -5,6 +5,37 @@ import math
 import copy
 import heapq
 import numpy as np
+import random
+
+array
+w
 
 def geneticAlg(b):
-    b
+    global array
+    global w
+
+    n = leb(b)
+    array = []
+    w = []
+    def makeBoardVector(b):
+        for i in range(n):
+            for j in range(n):
+                if b[j][i] != 0:
+                    array.append([i, j])
+                    w.append(b[j][i])
+
+    makeBoardVector(b)
+
+    def generateSuccessors(v):
+        for x in range(int(sqrt(v.len))):
+            i = random.randint(0, n - 1)
+
+
+    def swapColumn(v1, v2, col):
+        for i in range(len(v1)):
+            
+
+
+
+
+
