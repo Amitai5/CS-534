96c61ed8528ba1c9bb0d6bb2b675349b6ae12e51
adelinemae <98119483+adelinemae@users.noreply.github.com>
2022-02-21 01:12:14 -0500

    Update GeneticAlg.py

--- Assignment 1/GeneticAlg.py
+++ Assignment 1/GeneticAlg.py
@@ -77,25 +77,28 @@ def geneticAlg(b):
 
 
     def attacks(b):
+        size = n
         numAttacks = 0
-        for i in range(size):
+        for i in range(n):
             x0 = b[i][0]
             y0 = b[i][1]
-            for x in range(x0 + 1, size):
+            z = int(x0) + 1
+            for x in range(z, n):
                 if (x, y0) in b:
                     numAttacks += 1
                     # print("row")
-            for y in range(y0 + 1, size):
-                if (x0, y) in b:
-                    numAttacks += 1
+            for y in range(y0 + 1, n):
+                if [x0, y] in b:
+                    numAttacks = numAttacks + 1
                     # print("col")
-            for d in range(1, size):
-                if (x0 + d, y0 + d) in b:
-                    numAttacks += 1
+            for d in range(1, n):
+                if [x0 + d, y0 + d] in b:
+                    numAttacks = numAttacks + 1
                     # print("d+ "+str(x0+d)+ " "+str(y0+d))
-                if (x0 + d, y0 - d) in b:
-                    numAttacks += 1
+                if [x0 + d, y0 - d] in b:
+                    numAttacks = numAttacks + 1
                     # print("d- " + str(x0 + d) + " " + str(y0 - d))
+       #print(numAttacks)
 
         return numAttacks * 100
 
@@ -124,11 +127,15 @@ def geneticAlg(b):
         global best
         global elite
         elite = []
-        sortFit(ss)
+        #sortFit(ss)
+        #print("About to sort")
+        #print(ss)
+        ss.sort(key=lambda x: x[1], reverse=True)
+        #print(ss)
         for i in range(int(math.sqrt(len(ss)))):
             elite.append(copy.deepcopy(ss[i]))
         if elite[0][1] > best[1]:
-            best = elite[0]
+            best = copy.deepcopy(elite[0])
 
 
     def cull(): #remove the weakest pop
@@ -137,12 +144,27 @@ def geneticAlg(b):
 
     def checkElite(s):
         s[1] = fit(s[0])
-        print("elite: ", elite)
+        #print("elite: ", elite)
         if s[1] > elite[-1][1]:
             elite.insert(0, copy.deepcopy(s))
 
     def sortFit(arr):
-        ss.sort(key=lambda x: x[1])
+        return 0
+        #arr = zip(*arr)
+        #print("Hello, sorting!")
+        #arr.sort(key=lambda x: x[1])
+
+    def swapColumn(v1, v2, col):
+        #v3 = copy.deepcopy(v1)
+        #v4 = copy.deepcopy(v2)
+        v1c = []
+        v2c = []
+        v1c.append(v1[:][col])
+        v2c.append(v2[:][col])
+        for i in range(len(v1c)):
+            ti = copy.deepcopy(v1c[i])
+            v1c[i] = copy.deepcopy(v2c[i])
+            v2c = ti
 
     def scramble(s1, s2):
         random.randint(0, 100)
@@ -157,18 +179,16 @@ def geneticAlg(b):
                 mutateCol(s2[0], i)
             checkElite(s1)
             checkElite(s2)
-        s1[1] = fit(s1)
-        s2[1] = fit(s2)
+        s1[1] = fit(s1[0])
+        s2[1] = fit(s2[0])
 
         return s1, s2
 
     def mutateCol(v, col):
-        vs = v[:][col]
+        vs = []
+        vs.append(v[:][col])
         for i in vs:
             ni = random.randint(0, n - 1)  # new row position
-            print("v: ", v)
-            print("vs: ", vs)
-            print("i: ", i)
             newCoord = [ni, i[1]]
             if not (newCoord in v):
                 i = newCoord
@@ -178,33 +198,47 @@ def geneticAlg(b):
         string = ""
         for i in range(n):
             for j in range(n):
-                if (i, j) in v:
-                    string = string + " " + str(w[v.index((i, j))]) + " "
+                if [i, j] in v:
+                    string = string + " " + str(w[v.index([i, j])]) + " "
                 else:
                     string = string + " 0 "
                 if j == n - 1:
                     string = string + "\n"
         return string
 
+    def checkRealClose():
+        m = 0
+        for i in range(len(elite)):
+            m = m + elite[i][1]
+        m = m / len(elite)
+        if m == best[1]:
+            return True
+        else:
+            return False
+
     def recurs():
         global k
+        global ss
 
         setElite()
         cull()
         for i in range(k):
-            print("ss: ", ss)
+            #print("ss: ", ss)
             s1, s2 = random.choices(ss, k=2)
             scramble(s1, s2)
         k = k - 1
 
-        if k == 0 or (cost[best[0]] < 400 and k < sqrt(n)):
+        if k == 0 or (checkRealClose() and k < math.sqrt(n)): #or (numAttacks(best[0]) < 400 and k < math.sqrt(n))
             print("Finished")
+            print(best)
+            print("Initial fit: ", fit(array))
             print("Final fit: ", best[1])
             print("Final cost: ", cost(best[0]))
             print(vToBoard(best[0]))
         else:
-            ss.append(elite)
+            ss = ss + elite
             recurs()
+
     # n = len(b)
     array = b
     nq = len(w)
@@ -213,7 +247,7 @@ def geneticAlg(b):
     ss = []  # list of successors
     best = [array, fit(array)]
     ss.append(best)
-    k = n ** 2  # num successors
+    k = (n ** 2) * 4  # num successors
     generateSuccessors(array)
     recurs()
 
@@ -225,10 +259,6 @@ def geneticAlg(b):
 
     # v1: [i, j] [row, col] [[row1, col1] [row1, col1] [row 2, col2]]
     # swap columns without needing to return, ideally. TODO: check
-    def swapColumn(v1, v2, col):
-        v3 = copy.deepcopy(v1)
-        v4 = copy.deepcopy(v2)
-        v1[:, col] = v4[:, col]
-        v2[:, col] = v3[:, col]
+
 
 geneticAlg(boardArrayXY)
