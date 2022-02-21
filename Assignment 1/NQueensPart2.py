2e671a3f76a2b6eabc4677c92cdd7c149a789459
Amitai5 <aberfanian@outlook.com>
2022-02-21 05:27:05 -0500

    Finalized Program (FR)

--- Assignment 1/NQueensPart2.py
+++ Assignment 1/NQueensPart2.py
@@ -8,49 +8,49 @@ from matplotlib import pyplot as plt
 max_run_times = [0.1, 0.25, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20]
 
 
-# def create_graph(x_axis, genetics_costs, hillClimb_costs):
-#     print("Finished!")
-#     plt.plot(x_axis, genetics_costs, label="Genetics Algorithm")
-#     plt.plot(x_axis, hillClimb_costs, label="Hill Climbing")
-#     plt.xlabel('Run Time')
-#     plt.ylabel('Algorithm Cost')
-#     plt.title('Performance vs Run Time')
-#     plt.legend()
-#     plt.show()
-#
-#
-# print("---------------Test Results---------------\n")
-# # csvwrite98.create_board_csv(10, "board.txt", False) Create board when not given one...
-#
-# print("Testing HillClimber and Genetics for 2 minutes...")
-# HillClimbing.findSolution(120, False)
-# GeneticAlg.geneticAlg(120, False)
-# create_graph(range(1, 13), GeneticAlg.interval_cost_updates, HillClimbing.interval_cost_updates)
-#
-#
-# print("\nTesting Hill Climbing and Genetics for [0.1, 0.25, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20] second intervals...")
-# genetics_costs = []
-# hillClimb_costs = []
-# for run_time in tqdm(max_run_times):
-#     hillClimb_attack, hillClimb_cost = HillClimbing.findSolution(run_time, False)
-#     hillClimb_costs.append(hillClimb_cost)
-#
-#     genetics_fitness, genetics_cost = GeneticAlg.geneticAlg(run_time, False)
-#     genetics_costs.append(genetics_cost)
-#
-# create_graph(max_run_times, genetics_costs, hillClimb_costs)
+def create_graph(x_axis, genetics_costs, hillClimb_costs):
+    print("Finished!")
+    plt.plot(x_axis, genetics_costs, label="Genetics Algorithm")
+    plt.plot(x_axis, hillClimb_costs, label="Hill Climbing")
+    plt.xlabel('Run Time')
+    plt.ylabel('Algorithm Cost')
+    plt.title('Performance vs Run Time')
+    plt.legend()
+    plt.show()
+
+
+print("---------------Test Results---------------\n")
+# csvwrite98.create_board_csv(10, "board.txt", False) Create board when not given one...
+
+print("Testing HillClimber and Genetics for 2 minutes...")
+HillClimbing.findSolution(120, False)
+GeneticAlg.geneticAlg(120, False)
+create_graph(range(1, 13), GeneticAlg.interval_cost_updates, HillClimbing.interval_cost_updates)
+
+
+print("\nTesting Hill Climbing and Genetics for [0.1, 0.25, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20] second intervals...")
+genetics_costs = []
+hillClimb_costs = []
+for run_time in tqdm(max_run_times):
+    hillClimb_attack, hillClimb_cost = HillClimbing.findSolution(run_time, False)
+    hillClimb_costs.append(hillClimb_cost)
+
+    genetics_fitness, genetics_cost = GeneticAlg.geneticAlg(run_time, False)
+    genetics_costs.append(genetics_cost)
+
+create_graph(max_run_times, genetics_costs, hillClimb_costs)
 
 
 print("\nTesting largest board given 20 second Time Limit...")
 hillClimb_board_size = 10
-csvwrite98.create_board_csv(hillClimb_board_size, "board.txt", False)
+# csvwrite98.create_board_csv(hillClimb_board_size, "board.txt", False)
 _, hillClimb_cost = HillClimbing.findSolution(20, False)
 print("Hill Climbing")
 print("- Board Size: " + str(hillClimb_board_size))
 print("- Cost: " + str(hillClimb_cost))
 
-genetics_board_size = 15
-csvwrite98.create_board_csv(genetics_board_size, "board.txt", False)
+genetics_board_size = 14
+# csvwrite98.create_board_csv(genetics_board_size, "board.txt", False)
 _, genetics_cost = GeneticAlg.geneticAlg(20, False)
 print("Genetics Algorithm")
 print("- Board Size: " + str(genetics_board_size))
