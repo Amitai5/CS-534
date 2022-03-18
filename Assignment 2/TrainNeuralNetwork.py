77c0d1fa9ad27d5c4751a4c5a75c3b2d7fbc4e4b
wengdahl <wfengdahl@wpi.edu>
2022-03-18 02:54:29 -0400

    added features

--- Assignment 2/TrainNeuralNetwork.py
+++ Assignment 2/TrainNeuralNetwork.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas
 import tqdm
 import os
+import statistics
 
 training_data = []
 training_data_directory = os.getcwd() + "\\trainingData"
@@ -13,6 +14,36 @@ for name in os.listdir(training_data_directory):
         astar_cost = data[1]
 
         # Will extract features here
+        print(board)
+        print(len(board))
+        #Create features, first 12 are row of queen in column, weight of queen in row
+        totalWeight = 0
+        totalAttackWeight = 0
+        numAttQueens = 0
+        rowPos = []
+        heaviestAttacking = 0
+
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if board[j][i] != 0:
+                    features.append(j)
+                    features.append(board[j][i])
+                    totalWeight += board[j][i]
+                    rowPos.append(j)
+        for i in range(len(board)):
+            myRow = rowPos[i]
+            #check for attacks
+            for j in range(i+1,6):
+                if rowPos[j] == myRow or rowPos[j] == myRow+(j-i) or rowPos[j] == myRow-(j-i):
+                    numAttQueens+=1;
+                    heaviestAttacking = max(heaviestAttacking, board[myRow][i], board[rowPos[j]][j])
+                    totalAttackWeight += board[myRow][i] + board[rowPos[j]][j]
+        #Add last 5 derived features
+        features.append(totalAttackWeight/totalWeight)
+        features.append(numAttQueens)
+        features.append(statistics.stdev(rowPos))
+        features.append(heaviestAttacking)
+        features.append(totalWeight)
 
         sanitized_data = [features, astar_cost]
         training_data.append(sanitized_data)
