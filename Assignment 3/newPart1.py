c5567db4d4dd76fa95ea2e22118d9fccb4bbd5f7
adelinemae <98119483+adelinemae@users.noreply.github.com>
2022-04-05 15:21:16 -0400

    Update newPart1.py

--- Assignment 3/newPart1.py
+++ Assignment 3/newPart1.py
@@ -16,27 +16,32 @@ import copy
 # P(action succeeds):  the transition model.
 
 # Arg format: name of file, reward for action (non positive), gamma (discount), # of seconds to run, P(action succeeds)
-# e.g. grid_sample.txt -.1 .2 5 .8
+# e.g. grid_sample.txt -.1 .8 5 .8
 
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
 
 file = sys.argv[1]
-rew = sys.argv[2]
-gamma = sys.argv[3]
+rew = float(sys.argv[2])
+gamma = float(sys.argv[3])
 sec = float(sys.argv[4])
 P = float(sys.argv[5])
 Q = []
+heatmap = []
+count = 0
 
 # def takeAction(s, a):
 #     s
 
 def load_grid(filename):
     global Q
+    global heatmap
     grid = pd.read_csv(filename, delimiter='\t',header=None).to_numpy()
     Q = copy.deepcopy(grid)
+    heatmap = copy.deepcopy(grid)
     for i in range(len(grid)):
         for j in range(len(grid[i])):
+            heatmap[i][j] = 0
             if grid[i][j] == "S":
                 s = (i, j)
                 Q[i][j] = 0
@@ -44,7 +49,12 @@ def load_grid(filename):
                 x = (i, j)
             else:
                 Q[i][j] = int(grid[i][j])
+                grid[i][j] = int(grid[i][j])
     print("initial Q: \n", Q)
+    # heatmap = [0] * len(grid[0])
+    # heatmap = [heatmap] * len(grid)
+
+    print("heatmap: \n", heatmap)
 
     return grid, s, x
 
@@ -87,7 +97,7 @@ def determineAction(s):
 def tryA(a):
     tr = np.array(a)
     # print("tr: ", tr)
-    print("P: ", P)
+    # print("P: ", P)
     if random.random() > P:
         if random.random() > .5:
             tr = tr * 2
@@ -98,13 +108,16 @@ def tryA(a):
     return tr[0], tr[1]
 
 
-def takeAction(s, a):  # /* trickier :-) */
+def takeAction(s, a, bool=False):  # /* trickier :-) */
     # /* the ONLY PLACE the transition model should appear in your code */
     s0 = copy.deepcopy(s)
-    aa = tryA(a)  # actual action to take
+    if not bool:
+        aa = tryA(a)  # actual action to take
+    else:
+        aa = a
     x = s[0] + aa[0]
     y = s[1] + aa[1]
-    print("aa: ", aa)
+    # print("aa: ", aa)
     if (aa[0] == 2 or aa[1] == 2 or aa[0] == -2 or aa[1] == -2):
         x1 = s[0] + a[0]
         y1 = s[0] + a[1]
@@ -124,14 +137,20 @@ def update(s, a, s0):  # /* depends on SARSA vs Q-learning */
     global gamma #passed as parameter
     global rew
     alpha = 0.5 #learning rate - higher means faster
-    Qt=Q[s[0]][s[1]]#Current Q-value
-    Qnext = Q[s[0]+a[0]][s[1]+a[1]] #next reward estimate - from the Q-value of the square you want to be at
+    Qt=Q[s]#Current Q-value
+    s1 = takeAction(s, a, True)
+
+    a0 = determineAction(s0)
+
+
+    # Qnext = Q[s1] #next reward estimate - from the Q-value of the square you want to be at
+    Qnext = q(s0, a0)
     Qmaxfuture = Qnext #need to calcualte for Q-learning, maximum future reward - currently estimate for testing
 
     SARSA = 1#set to 1 for SARSA, 2 for Q-learning - I think SARSA should run, but Q-learning is not fully implemented
     if SARSA:
         #SARSA
-        Q[s[0]][s[1]] = Qt + alpha*(rew + gamma*Qnext-Qt)
+        Q[s] = Qt + alpha*(rew + gamma*Qnext-Qt)
     else:
         #Q learning
         Q[s[0]][s[1]] = Qt + alpha*(rew + gamma*Qmaxfuture-Qt)
@@ -139,10 +158,26 @@ def update(s, a, s0):  # /* depends on SARSA vs Q-learning */
 
 def notTerminal(s):
     # print("nt: ", board[s])
-    return board[s] == 0
+    if(board[s] == 'X'):
+        print("Board(s): ", board[s], "\nSomething is wrong")
+    return board[s] == 0 or board[s] == 'S'
+
+
+def printArray(arr):
+    string = ""
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if arr[i, j] != 'X':
+                string = string + str(np.round(float(arr[i, j]), 4))
+            else:
+                string += str(arr[i, j])
+            string += "\t"
+        string = string + "\n"
+    print(string)
 
 
 def rl():
+    global count
     startTime = time.time()
     while time.time() - startTime < sec:
         s = startState
@@ -150,7 +185,18 @@ def rl():
             a = determineAction(s)
             s0 = takeAction(s, a)
             update(s, a, s0)
+            heatmap[s0] += 1
+            count = count + 1
             s = s0
+            # print("Q: \n", Q)
+    print("Q: \n")
+
+    printArray(Q)
+    print("Heatmap: \n")
+    for i in range(len(heatmap)):
+        for j in range(len(heatmap[0])):
+            heatmap[i, j] = heatmap[i, j] / count
+    printArray(heatmap)
 
 
 
@@ -161,9 +207,12 @@ board, startState, my = load_grid(file)
 rl()
 
 
-a = determineAction((0, 0))
-print("Action: ", a)
-print("s0: ", takeAction((0, 0), a))
+
+
+
+# a = determineAction((0, 0))
+# print("Action: ", a)
+# print("s0: ", takeAction((0, 0), a))
 
 # print("Bool: ", notTerminal((0,1)))
 
