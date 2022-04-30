ac054c1a1a9cb666fe70185c46dff3a7b6f795b7
Amitai5 <aberfanian@outlook.com>
2022-04-30 15:53:21 -0400

    Small Changes

--- Final Project/main.py
+++ Final Project/main.py
@@ -9,6 +9,7 @@ model.eval()
 img = cv2.imread(os.curdir + "\\test_images\\test.png")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+results = []
 for i in range(0, 8):
     x_index = i * 29
     cropped_img = img[0:28, x_index:x_index+28]
@@ -16,4 +17,6 @@ for i in range(0, 8):
     if not helper.is_image_empty(cropped_img):
         cropped_img = img2tensor(cropped_img)
         result = model(cropped_img)[0]
-        print(helper.clean_results(result))
+        result = helper.clean_results(result)
+        results.append(result)
+
