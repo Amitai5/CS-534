471b0817902945f6cbdf9d96dc8a894b6c0e5538
adelinemae <98119483+adelinemae@users.noreply.github.com>
2022-04-30 15:51:41 -0400

    update

--- Final Project/SSAlg.py
+++ Final Project/SSAlg.py
@@ -1,4 +1,7 @@
 dict = {}
+baseString = ""
+
+
 def dictInit():
     global dict
     text_file = open("data/Dictionary/oxford_dictionary_words.txt", "r")
@@ -11,7 +14,53 @@ def dictInit():
     dict = set(lines)
     print(dict)
 
+
 def sSpellAlg(word):
-    X
+    global baseString
+    # [ [ ( float, char ), ( float, char ) ] [ ( float, char ), ( float, char ) ] ]
+    # [ [ ( float, char ) ] ]
+    # List of positions where each position has a list of ordered pairs where each order pair contains a
+    # float percentage and a char value
+    # eg = [[(1, 'a'), (.87, 'b'), (.32, 'c')], [(1, 'e'), (.62, 'f')], [(1, 'g'), (.33, 'h')]]
+    string = ""
+    arr = []
+    print(word)
+    for i in range(len(word)):
+        pos = word[i]
+        string = string + pos.pop(0)[1]
+        for j in range(len(pos)):
+            arr.append((pos[j][0], pos[j][1], i))
+
+
+    print(string)
+    baseString = string
+    arr.sort(key=lambda x: x[0], reverse=True)
+    print(arr)
+    print("result: ", spellCheck(string, arr))
+
+
+def spellCheck(string, todo):
+    if string in dict or not todo:
+        return string
+    else:
+        return spellCheck(replaceNext(baseString, todo), todo)
+
+
+def replaceNext(string, todo):
+    if todo:
+        newC = todo.pop(0)
+        print("NewC: ", newC)
+        index = newC[2]
+        newstring = newC[1]
+        string = string[:index] + newstring + string[index + 1:]
+        print(string)
+        return string
+    else:
+        return string
+
 
 dictInit()
+
+eg = [[(1, 'a'), (.34, 'b'), (.32, 'c')], [(1, 'e'), (.62, 'f')], [(1, 'g'), (.33, 'h')]]
+
+sSpellAlg(eg)
